5960d82c580e96cf55b7bb0e4af1782b96d101ec
Pedro Ivan Fernandez Gonzalez <pedroifgonzalez@gmail.com>
2022-02-22 14:36:21 -0500

    Refactor code and add README

---
+++ refunds/README.md
@@ -0,0 +1,54 @@
+# *Viajando Anuncia* Channel's Data Collector
+
+## Retrieve and filter travel's refunds
+
+## Pre-requisites
+
+In order to use this script you must have installed Python 3.8 or greater
+
+## Clone repository
+
+````text
+git clone path/to/url/project
+````
+
+## Configuration
+
+### Create a virtual enviroment
+
+````text
+python3 -m venv venv
+````
+
+### Activate it
+
+````text
+source venv/bin/activate
+````
+
+### Install requirements
+
+````text
+pip install -r refunds/requirements/base.txt
+````
+
+### Usage
+
+Before running the script you will need a Telegram API id and hash to sign in. Follow the instructions given [here](https://docs.telethon.dev/en/stable/basic/signing-in.html)
+
+Then create a `.env` file in `refunds` folder following the variables as the `env_example` file:
+
+````.env
+APP_API_ID=your_api_id
+APP_API_HASH=your_api_hash
+````
+
+Now, you can run the script:
+
+````text
+python refunds/datacollector.py
+````
+
+## Use case
+
+You may want use the script to get notified when a specific travel origin and destination are founded
--- refunds/datacollector.py
+++ refunds/datacollector.py
@@ -1,16 +1,15 @@
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict
 
 from dotenv import dotenv_values
 from telethon import TelegramClient, events
-
 from utils import filter_travels, parse_refunds
 
 settings = dotenv_values(str(Path(__file__).parent) + os.sep + ".env")
 api_id = int(settings.get("APP_API_ID") or 0)
 api_hash = str(settings.get("APP_API_HASH"))
-app_title = str(settings.get("APP_TITLE"))
+app_title = str(settings.get("APP_TITLE", "datacollector"))
 
 if not(api_id and api_hash and app_title):
     raise Exception("Missing env variables")
@@ -21,8 +20,11 @@ client = TelegramClient(app_title, api_id=api_id, api_hash=api_hash)
 async def my_event_handler(event: Any):
     if "Reintegros" in event.raw_text:
         travels = parse_refunds(event.raw_text)
+        # Add filters here (check Travel class attributes)
+        # origin="La Habana", day_of_week="sáb"
+        filters: Dict[str, Any] = dict()
         filtered_travels = filter_travels(
-            travels, origin="La Habana", day_of_week="sáb"
+            travels, **filters
         )
         print(filtered_travels)
 
